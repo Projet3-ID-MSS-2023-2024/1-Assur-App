d91cebb118bd897707235519d8cb5c460eb8c1ba
Assabban Yassin <la200036@student.helha.be>
2023-11-14 19:21:50 +0100

    Fix: table name users

--- assurapp-api/src/main/java/be/helha/assurappapi/authentification/models/User.java
+++ assurapp-api/src/main/java/be/helha/assurappapi/authentification/models/User.java
@@ -3,14 +3,17 @@ package be.helha.assurappapi.authentification.models;
 import be.helha.assurappapi.authentification.interfaces.UserInterface;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Entity
+@Table(name="users")
 @Data @AllArgsConstructor @NoArgsConstructor
 public class User implements UserInterface{
+
     @Id
     private Long id;
     @NonNull
