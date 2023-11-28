e8d3c818709f32cec3dfa4ea9b001cdfd7c79344
therifian <therifianeagle@gmail.com>
2023-11-28 19:09:29 +0100

    feat: home page completed
    completed home page with lorem ipsum text

--- assurapp-web/src/app/components/home/home.component.ts
+++ assurapp-web/src/app/components/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterLink} from "@angular/router";
+import {FooterComponent} from "./footer/footer.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ItemComponent} from "./item/item.component";
+import {CardComponent} from "./card/card.component";
+import {CartXlComponent} from "./cart-xl/cart-xl.component";
+import {ContactFormComponent} from "./contact-form/contact-form.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FooterComponent, ReactiveFormsModule, ItemComponent, CardComponent, CartXlComponent, ContactFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
