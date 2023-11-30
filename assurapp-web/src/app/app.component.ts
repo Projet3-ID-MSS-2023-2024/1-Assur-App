3426cd174abf694956d03877c12119c4cd4b9646
therifian <therifianeagle@gmail.com>
2023-11-30 13:35:47 +0100

    feat: insurances dashboard page
    dashboard page for insurer with:
    - insurance list
    - add new one
    - update
    - delete

--- assurapp-web/src/app/app.component.ts
+++ assurapp-web/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {InsuranceComponent} from "./components/insurance/insurance.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SidebarComponent, InsuranceComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
