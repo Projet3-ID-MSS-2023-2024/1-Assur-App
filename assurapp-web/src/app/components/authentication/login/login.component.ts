b69dbe3da38435192a09e0caef09ac45596bbde7
Khattabi Oussama la216646 <la216646@student.helha.be>
2023-12-28 13:14:23 +0100

    feat: role adding, token decoder + token data getter

--- assurapp-web/src/app/components/authentication/login/login.component.ts
+++ assurapp-web/src/app/components/authentication/login/login.component.ts
@@ -1,15 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {CommonModule, DOCUMENT} from '@angular/common';
-import {RegisterService} from "../../../services/authentication/register.service";
-import {MessageService} from "primeng/api";
+import {CommonModule} from '@angular/common';
 import {User} from "../../../interfaces/user";
 import {LoginService} from "../../../services/authentication/login.service";
 import {ToastModule} from "primeng/toast";
 import {FormsModule} from "@angular/forms";
 import {MessageModule} from "primeng/message";
 import {MessagesModule} from "primeng/messages";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {NavbarComponent} from "../../home/navbar/navbar.component";
+import {TokenService} from "../../../services/token.service";
 
 @Component({
   selector: 'app-login',
@@ -17,11 +16,10 @@ import {NavbarComponent} from "../../home/navbar/navbar.component";
   imports: [CommonModule, ToastModule, MessageModule, MessagesModule, FormsModule, RouterLink, NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  providers: [MessageService]
 })
 export class LoginComponent implements OnInit{
   user! : User;
-  constructor(private loginService: LoginService, private messageService: MessageService) {
+  constructor(private loginService: LoginService, private tokenService: TokenService, private router : Router) {
   }
 
   ngOnInit(): void {
@@ -35,18 +33,28 @@ export class LoginComponent implements OnInit{
     }
   }
 
-  login(){
+  onSubmit(){
     this.user.id = 0;
     this.loginService.login(this.user.email, this.user.password).subscribe({
       next:(data)=>{
-        console.log(data);
-        this.messageService.add({severity:'success', summary: 'Success', detail: 'User authenticated'})
+        //console.log(data);
+        this.tokenService.saveToken(data.bearer)
 
+        //this.router.navigate([''])
       },
       error:(error) =>{
         console.log(error);
-        this.messageService.add({severity:'error', summary: 'Error', detail: 'Error occured'})
       }
     })
+  }
+
+  test(){
+    console.log(this.tokenService.getUserRole())
+    console.log("-----------")
+    const data = JSON.parse(atob("eyJzdWIiOiJhbGljZS5zbWl0aEBleGFtcGxlLmNvbSIsImV4cCI6MTcwMzc1MTU4Nywicm9sZSI6eyJpZCI6MSwibGFiZWwiOiJDTElFTlQifSwiaWQiOjEsIm5hbWUiOiJhIn0"))
+
+    console.log(data)
+    console.log("-----------")
+    console.log(data.sub)
   }
 }
