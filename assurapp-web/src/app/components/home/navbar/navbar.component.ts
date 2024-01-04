046256e5cb616e3a4f16d88400a9dfdc6ed64248
Khattabi Oussama la216646 <la216646@student.helha.be>
2024-01-04 04:03:14 +0100

    feat: auto logout due to inactivity(15 minutes)

--- assurapp-web/src/app/components/home/navbar/navbar.component.ts
+++ assurapp-web/src/app/components/home/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {NgIf, NgOptimizedImage} from "@angular/common";
 import {Router, RouterLink} from "@angular/router";
 import {AuthenticationService} from "../../../services/authentication.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-navbar',
@@ -16,17 +17,44 @@ import {AuthenticationService} from "../../../services/authentication.service";
 })
 export class NavbarComponent implements OnInit{
   isLogged:boolean = false;
-  constructor(private authService: AuthenticationService, private route: Router) {
+  timeBeforeLogout : number = 900;
+  interval: number | undefined;
+  subscribe !: Subscription;
 
+  constructor(private authService: AuthenticationService, private route: Router) {
   }
 
   ngOnInit(): void {
     this.isLogged = this.authService.isLogged();
+    this.startTimer()
   }
 
   logout(){
     this.authService.logout();
     location.reload()
+  }
+
+
+  @HostListener('window:keypress', ["$event"])
+  @HostListener('window:beforeunload', ["$event"])
+  @HostListener('window:mousemove', ["$event"])
+  mouseMoveWatcher(event:Event){
+    this.resetTimer()
+  }
+
+  startTimer(){
+    this.interval = setInterval(()=>{
+      if(this.timeBeforeLogout > 0){
+        this.timeBeforeLogout--;
+      } else if(this.timeBeforeLogout == 0 && this.isLogged){
+        this.authService.logout()
+        location.reload()
+      }
+    }, 1000)
+  }
+
+  resetTimer(){
+    this.timeBeforeLogout = 900;
   }
 
 }
